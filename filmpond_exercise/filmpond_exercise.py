65783b18c93b6ee8172558b251ef3b34de3f7298
anna <annateresavillanueva@gmail.com>
2016-03-08 20:56:33 +1100

    added a function to get connections of a position in a map

--- filmpond_exercise/filmpond_exercise.py
+++ filmpond_exercise/filmpond_exercise.py
@@ -1,3 +1,18 @@
 class FilmpondChallenge:
     def foo(self):
         return 'bar'
+    
+    def get_connections(self, map, position, res):
+        row = position[0]
+	column = position[1]
+
+	res[position] = [ 
+	    (row - 1, column - 1),
+	    (row - 1, column),
+	    (row - 1, column + 1),
+	    (row, column + 1),
+	    (row + 1, column + 1),
+	    (row + 1, column),
+	    (row + 1, column - 1),
+	    (row, column - 1)
+	 ]
--- test/test_filmpond_exercise.py
+++ test/test_filmpond_exercise.py
@@ -5,5 +5,19 @@ from filmpond_exercise.filmpond_exercise import FilmpondChallenge
 class TestFilmpondChallenge(unittest.TestCase):
     def test_foo(self):
         fp = FilmpondChallenge()
-	res = fp.foo() 
-	self.assertEqual(res, 'bar')
+        res = fp.foo() 
+        self.assertEqual(res, 'bar')
+    
+    def test_sample_map(self):
+        map = [
+            ['+','+','+'],
+            ['+','+','+'],
+            ['+','+','+']
+        ]
+        fp = FilmpondChallenge()
+        res = {}
+        fp.get_connections(map, (1,1), res)
+        self.assertEqual(res, { 
+                                (1,1): [(0,0),(0,1),(0,2),(1,2),(2,2),(2,1),(2,0),(1,0)]
+                              }
+                        )
